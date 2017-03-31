3d2d90135435f65c480a14bfa4d990c4952d60a3
dddddtcare <jaychou0801@gmail.com>
2017-03-31 11:37:52 -0700

    .
    .

---
+++ bubble.java
@@ -0,0 +1,47 @@
+//bubble
+/*
+3,60,35,2,45,320,5
+3,60,35,2,45,320,5
+3,35,60,2,45,320,5
+3,35,2,60,45,320,5
+3,35,2,45,60,320,5
+3,35,2,45,60,320,5
+3,35,2,45,60,5,320
+
+Ω(n)    Θ(n^2)  O(n^2)  O(1)
+*/
+public class BubbleSortExample {  
+    static void bubbleSort(int[] arr) {  
+        int n = arr.length;  
+        int temp = 0;  
+         for(int i=0; i < n; i++){  
+                 for(int j=1; j < (n-i); j++){  
+                          if(arr[j-1] > arr[j]){  
+                                 //swap elements  
+                                 temp = arr[j-1];  
+                                 arr[j-1] = arr[j];  
+                                 arr[j] = temp;  
+                         }  
+                          
+                 }  
+         }  
+  
+    }  
+    public static void main(String[] args) {  
+                int arr[] ={3,60,35,2,45,320,5};  
+                 
+                System.out.println("Array Before Bubble Sort");  
+                for(int i=0; i < arr.length; i++){  
+                        System.out.print(arr[i] + " ");  
+                }  
+                System.out.println();  
+                  
+                bubbleSort(arr);//sorting array elements using bubble sort  
+                 
+                System.out.println("Array After Bubble Sort");  
+                for(int i=0; i < arr.length; i++){  
+                        System.out.print(arr[i] + " ");  
+                }  
+   
+        }  
+}  
